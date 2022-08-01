da04faa958f4505dcfb93bc6ef6bbee36159a2bb
Txema Serrano <jsersan@gmail.com>
2022-08-01 17:57:05 +0200

    first commit

---
+++ src/components/MiComponente.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id ="mi-componente">
+        <h1>Este es mi componente</h1>
+        <h2>{{subtitulo}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'miComponente',
+    data() {
+        return {
+            subtitulo: ' Soy el subtítulo de mi componente'
+        }
+    }
+}
+</script>
